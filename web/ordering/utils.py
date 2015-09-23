47c040c42bd67c73da143da8cb1e86ba5bc5bc25
= <dhill@usgs.gov>
2015-09-23 12:35:25 -0500

    fixed bug in utils.py

--- web/ordering/utils.py
+++ web/ordering/utils.py
@@ -1,6 +1,6 @@
 ''' Utility module '''
 
-def dictfetchall(self, cursor):
+def dictfetchall(cursor):
     ''' Returns all rows from a cursor as a dict '''
     desc = cursor.description
     return [
