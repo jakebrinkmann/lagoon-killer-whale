52300312fe9e36f859eaa9f86595c844b950bc42
dhill-usgs <dhill@usgs.gov>
2015-09-23 12:23:22 -0500

    renamed util to utils

---
+++ web/ordering/utils.py
@@ -0,0 +1,9 @@
+''' Utility module '''
+
+def dictfetchall(self, cursor):
+    ''' Returns all rows from a cursor as a dict '''
+    desc = cursor.description
+    return [
+            dict(zip([col[0] for col in desc], row))
+            for row in cursor.fetchall() ]
+
