1f46824a2b71bf3f8d8e2c4fc5e8365961a21c6b
dhill-usgs <dhill@usgs.gov>
2015-11-04 09:39:47 -0600

    updated scene_report with all time items and current items

--- web/ordering/sql/scene_report.sql
+++ web/ordering/sql/scene_report.sql
@@ -1,1 +1,5 @@
+# current items only
 select count(s.name) "Total Scenes", SUM(case when s.status in ('complete', 'unavailable') then 1 else 0 end) "Complete",  SUM(case when s.status not in ('complete', 'unavailable') then 1 else 0 end) "Processing",   u.email, u.first_name, u.last_name from ordering_scene s, ordering_order o, auth_user u where s.order_id = o.id and o.user_id = u.id and s.status != 'purged' group by u.email, u.first_name, u.last_name order by "Total Scenes" DESC;
+
+# all time items
+select count(s.name) "Total Scenes", SUM(case when s.status in ('purged', 'complete', 'unavailable') then 1 else 0 end) "Complete",  SUM(case when s.status not in ('purged', 'complete', 'unavailable') then 1 else 0 end) "Processing",   u.email, u.first_name, u.last_name from ordering_scene s, ordering_order o, auth_user u where s.order_id = o.id and o.user_id = u.id   group by u.email, u.first_name, u.last_name order by "Total Scenes" DESC;
