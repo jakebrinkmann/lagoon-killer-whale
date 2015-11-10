ffa03a0b32c3e80386e7bda30964f4c5c63ecb36
David Hill <dhill@usgs.gov>
2015-11-09 21:38:00 -0600

    added navbar stats

--- web/espa_web/context_processors.py
+++ web/espa_web/context_processors.py
@@ -1,5 +1,28 @@
 from ordering.models.configuration import Configuration as config
+from django.db import connection
+from django.core.cache import cache
+from datetime import datetime, timedelta
+from ordering.utilities import dictfetchall
 
+
+def scene_stats(request):
+    '''Includes stats for scene backlog and completed 24 hrs'''
+    context = {}
+    cutoff = datetime.now() - timedelta(hours=24)
+    stats = {'stats_current_backlog': "select count(*) \"count\" from ordering_scene where status not in ('purged', 'complete', 'unavailable')",
+            'stats_completed_last_day': "select count(*) \"count\" from ordering_scene where completion_date >= now()::date - interval '24 hours'"}
+
+    for key in stats.keys():
+        count = cache.get(key)
+        if count is None:
+            with connection.cursor() as cursor:
+                cursor.execute(stats[key])
+                count = dictfetchall(cursor)[0]['count']
+                if count is None or count < 0:
+                    count = 0
+                cache.set(key, count, 120)
+        context[key] = count
+    return context
 
 def include_external_urls(request):
     '''Django context processor to include needed urls in the
--- web/espa_web/settings.py
+++ web/espa_web/settings.py
@@ -124,31 +124,6 @@ USE_TZ = False
 STATIC_ROOT = os.path.join(BASE_DIR, 'espa_web', 'static/')
 STATIC_URL = '/static/'
 
-# Templates
-#EMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates"
-    #or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-#   os.path.join(BASE_DIR, "espa_web/templates"),
-#
-
-#EMPLATE_CONTEXT_PROCESSORS = (
-#   'django.core.context_processors.debug',
-#   'django.core.context_processors.i18n',
-#   'django.core.context_processors.media',
-#   'django.core.context_processors.static',
-#   'django.contrib.auth.context_processors.auth',
-#   'django.contrib.messages.context_processors.messages',
-#   'espa_web.context_processors.include_external_urls',
-#
-
-# List of callables that know how to import templates from various sources.
-#TEMPLATE_LOADERS = (
-#   'django.template.loaders.filesystem.Loader',
-#   'django.template.loaders.app_directories.Loader',
-#
-
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -163,6 +138,7 @@ TEMPLATES = [
                 'django.template.context_processors.tz',
                 'django.contrib.messages.context_processors.messages',
                 'espa_web.context_processors.include_external_urls',
+                'espa_web.context_processors.scene_stats',
             ],
         },
     },
