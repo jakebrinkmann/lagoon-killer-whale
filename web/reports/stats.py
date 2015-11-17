748dc315b4de4c28acc0037bc3d1ef40f441577d
dhill-usgs <dhill@usgs.gov>
2015-11-17 16:35:57 -0600

    added stat placeholders

--- web/reports/stats.py
+++ web/reports/stats.py
@@ -10,6 +10,21 @@ Created on Fri Nov 13 10:07:52 2015
 
 # show disk space on cache
 # show number of users with open orders
+#'''SELECT COUNT(DISTINCT user_id) FROM ordering_order o WHERE o.status = 'ordered' '''
+# show number of open orders
+#'''SELECT COUNT(orderid) FROM ordering_order WHERE status = 'ordered' '''
 # show backlog depth
+#'''SELECT COUNT(*) FROM ordering_scene WHERE status NOT IN ('purged', 'complete', 'unavailable')'''
 # show products completed 24 hrs
-# etc
+#'''SELECT COUNT(*) FROM ordering_scene s WHERE s.status = 'complete'  AND  completion_date > now() - interval '24 hours' '''
+
+# show duplicate scene count
+#'duplicate_scene_percentage': {
+#                'display_name': 'Duplicate Scenes (Percentage)',
+#                        'description': 'Displays the percentage of scenes that have been requested more than once',
+#                                'query': r'''SELECT
+#                                                     (1 - (count(distinct name)::float / count(name)::float) * 100) "Duplicate Scenes"
+#                                                                          FROM ordering_scene'''
+                                                                            
+
+
