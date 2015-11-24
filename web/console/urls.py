9f8a7fd917c8106648c900162ccb1222c180da12
= <dhill@usgs.gov>
2015-11-24 15:18:32 -0600

    restricted console to staff only

--- web/console/urls.py
+++ web/console/urls.py
@@ -1,15 +1,20 @@
 from django.conf.urls import patterns, url
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required as login
+from django.contrib.admin.views.decorators import staff_member_required as staff
 
 from views import Index, StatusMessage, Config
 
 urlpatterns = patterns('', 
     url(r'^config$',
-        login_required(Config.as_view()), name='console_config_listing'),
+        login(staff(Config.as_view(), login_url='login')), 
+            name='console_config_listing'),
     url(r'^config/(?P<key>.+)',
-        login_required(Config.as_view()), name='console_config'),
+        login(staff(Config.as_view(), login_url='login')), 
+            name='console_config'),
     url(r'^statusmsg',
-        login_required(StatusMessage.as_view()), name='console_statusmsg'),
+        login(staff(StatusMessage.as_view(), login_url='login')), 
+            name='console_statusmsg'),
     url(r'^$',
-        login_required(Index.as_view()), name='console_index')
+        login(staff(Index.as_view(), login_url='login')), 
+            name='console_index')
     )
