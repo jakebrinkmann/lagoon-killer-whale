55077ea7a6e2065f1b32a25f188cdf813737ce9b
clay austin <cgaustin@usgs.gov>
2016-04-01 11:32:21 -0500

    switch session storage to memcache, add login_required, add statistics retrieval

--- src/app.py
+++ src/app.py
@@ -1,26 +1,28 @@
-from flask import Flask
-from flask import request
-from flask import flash
-from flask import session
-from flask import redirect
+from flask import Flask, request, flash, session, redirect, render_template, url_for
+# OrderedDict and datetime used by reports, leave
+from collections import OrderedDict
+import datetime
 from flask.ext.session import Session
-from flask import g
-from flask import render_template
-from flask import url_for
-
+from functools import wraps
 from user import User
-
 import requests
 import json
 
 app = Flask(__name__)
 app.config.from_envvar('ESPAWEB_SETTINGS', silent=False)
 app.secret_key = '@ijn@@d)h@8f8avh+h=lzed2gy=hp2w+6+nbgl2sdyh$!x!%3+'
-app.config['SESSION_TYPE'] = 'filesystem'
+app.config['SESSION_TYPE'] = 'memcached'
 
 Session(app)
-
 api_base_url = "http://{0}:{1}".format(app.config['APIHOST'], app.config['APIPORT'])
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'user' not in session.keys():
+            return redirect(url_for('login', next=request.url))
+        return f(*args, **kwargs)
+    return decorated_function
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -30,6 +32,7 @@ def login():
         auth_url = api_base_url + "/api/v0/user"
         response = requests.get(auth_url, auth=(request.form['username'], request.form['password']))
         resp_json = json.loads(response._content)
+        resp_json['wurd'] = request.form['password']
         user = User(**resp_json)
         if response.status_code == 200:
             session['logged_in'] = True
@@ -43,20 +46,24 @@ def login():
 @app.route('/logout')
 def logout():
     session.pop('logged_in', None)
+    session.pop('user', None)
     flash('You were logged out')
     return redirect(url_for('login'))
 
 @app.route('/')
 @app.route('/index/')
+@login_required
 def index():
-    status_response = requests.get(api_base_url + '/api/v0/system-status')._content
+    user = session['user']
+    status_response = requests.get(api_base_url + '/api/v0/system-status', auth=(user.username, user.wurd))._content
     sys_msg_resp = json.loads(status_response)
     system_message_body = sys_msg_resp['system_message_body']
     system_message_title = sys_msg_resp['system_message_title']
-    if 'user' in session.keys():
-        user = session['user']
-    else:
-        user = None
+
+    stat_products_complete_24_hrs = requests.get(api_base_url + '/api/v0/statistics/stat_products_complete_24_hrs', auth=(user.username, user.wurd))._content
+    stat_backlog_depth = requests.get(api_base_url + '/api/v0/statistics/stat_backlog_depth', auth=(user.username, user.wurd))._content
+
+
     if system_message_title or system_message_body:
         display_system_message = True
     else:
@@ -65,10 +72,13 @@ def index():
                            display_system_message=display_system_message,
                            system_message_body=system_message_body,
                            system_message_title=system_message_title,
+                           stat_products_complete_24_hrs=stat_products_complete_24_hrs,
+                           stat_backlog_depth=stat_backlog_depth,
                            user=user
                            )
 
 @app.route('/ordering/new/')
+@login_required
 def new_order():
     status_response = requests.get(api_base_url + '/api/v0/system-status')._content
     sys_msg_resp = json.loads(status_response)
@@ -90,16 +100,19 @@ def new_order():
 
 @app.route('/ordering/status/')
 @app.route('/ordering/status/emailaddr')
+@login_required
 def list_orders():
     user = session['user']
     return render_template('list_orders.html', user=user)
 
 @app.route('/ordering/status/orderid')
+@login_required
 def view_order():
     user = session['user']
     return render_template('view_order.html', user=user)
 
 @app.route('/reports/')
+@login_required
 def list_reports():
     req_url = api_base_url + "/api/v0/reports/"
     response = requests.get(req_url)
@@ -108,6 +121,7 @@ def list_reports():
     return render_template('list_orders.html', reports=res_data, user=user)
 
 @app.route('/reports/<name>/')
+@login_required
 def show_report(name):
     req_url = api_base_url + "/api/v0/reports/{0}/".format(name)
     response = requests.get(req_url)
