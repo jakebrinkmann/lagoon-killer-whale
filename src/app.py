c21765f91a10a1b375fee198079b7ccd211af6c2
clay austin <cgaustin@usgs.gov>
2016-04-14 15:10:11 -0500

    code cleanup, dry-ing

--- src/app.py
+++ src/app.py
@@ -1,13 +1,12 @@
 from flask import Flask, request, flash, session, redirect, render_template, url_for
-# OrderedDict and datetime used by reports, leave
+# OrderedDict and datetime used by reports
+# leave these imports
 from collections import OrderedDict
 import datetime
 from datetime import timedelta
 from flask.ext.session import Session
-from werkzeug.datastructures import ImmutableMultiDict
 from functools import wraps
-from user import User
-from utils import conversions, deep_update, is_num
+from utils import conversions, deep_update, is_num, gen_nested_dict, User, format_errors
 import requests
 import json
 
@@ -20,18 +19,33 @@ app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(minutes=30)
 Session(app)
 api_base_url = "http://{0}:{1}".format(app.config['APIHOST'], app.config['APIPORT'])
 
+
+def api_get(url, response_type='json', json=None, uauth=None):
+    auth_tup = uauth if uauth else (session['user'].username, session['user'].wurd)
+    response = requests.get(api_base_url + url, auth=auth_tup, json=json)
+    if response_type == 'json':
+        _resp = response.json()
+    elif response_type == 'text':
+        _resp = response.text
+    else:
+        _resp = response
+    return _resp
+
+
+def api_post(url, json):
+    auth_tup = (session['user'].username, session['user'].wurd)
+    response = requests.post(api_base_url+url, json=json, auth=auth_tup)
+    return response
+
+
 def update_status_details():
-    user = session['user']
-    status_response = requests.get(api_base_url + '/api/v0/system-status',
-                                   auth=(user.username, user.wurd))
-    sys_msg_resp = status_response.json()
-    session['system_message_body'] = sys_msg_resp['system_message_body']
-    session['system_message_title'] = sys_msg_resp['system_message_title']
-    session['display_system_message'] = sys_msg_resp['display_system_message']
-    comp_url = api_base_url + '/api/v0/statistics/stat_products_complete_24_hrs'
-    session['stat_products_complete_24_hrs'] = requests.get(comp_url, auth=(user.username, user.wurd)).text
-    depth_url = api_base_url + '/api/v0/statistics/stat_backlog_depth'
-    session['stat_backlog_depth'] = requests.get(depth_url, auth=(user.username, user.wurd)).text
+    status_response = api_get('/api/v0/system-status')
+    for item in ['system_message_body', 'system_message_title', 'display_system_message']:
+        session[item] = status_response[item]
+
+    for item in ['stat_products_complete_24_hrs', 'stat_backlog_depth']:
+        session[item] = api_get('/api/v0/statistics/' + item, 'text')
+
 
 def login_required(f):
     @wraps(f)
@@ -41,21 +55,16 @@ def login_required(f):
         return f(*args, **kwargs)
     return decorated_function
 
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    error = None
-    user = None
     next = request.args.get('next')
-
     if request.method == 'POST':
-        auth_url = api_base_url + "/api/v0/user"
-        response = requests.get(auth_url, auth=(request.form['username'], request.form['password']))
-        resp_json = response.json()
-        resp_json['wurd'] = request.form['password']
-        user = User(**resp_json)
-        if response.status_code == 200:
+        resp_json = api_get("/api/v0/user", uauth=(request.form['username'], request.form['password']))
+        if 'username' in resp_json.keys():
             session['logged_in'] = True
-            session['user'] = user
+            resp_json['wurd'] = request.form['password']
+            session['user'] = User(**resp_json)
             update_status_details()
 
             # send the user back to their
@@ -64,10 +73,13 @@ def login():
                 return redirect(next)
             else:
                 return redirect(url_for('index'))
-
         else:
-            flash(resp_json['msg'], 'error')
-    return render_template('login.html', error=error, user=user, next=next)
+            flash(format_errors(resp_json['msg']), 'error')
+    else:
+        if 'user' not in session.keys():
+            session['user'] = None
+    return render_template('login.html', next=next)
+
 
 @app.route('/logout')
 def logout():
@@ -76,51 +88,40 @@ def logout():
         session.pop(item, None)
     return redirect(url_for('login'))
 
+
 @app.route('/')
 @app.route('/index/')
 @login_required
 def index():
-    user = session['user']
-    return render_template('index.html', user=user)
+    return render_template('index.html')
+
 
 @app.route('/ordering/new/')
 @login_required
 def new_order():
-    user = session['user']
-    form_action = '/ordering/submit/'
-    return render_template('new_order.html',
-                           form_action=form_action, user=user)
-
+    return render_template('new_order.html', form_action=url_for('submit_order'))
 
 
 @app.route('/ordering/submit/', methods=['POST'])
 @login_required
 def submit_order():
-    user = session['user']
     # form values come in as an ImmutableMultiDict
     data = request.form.to_dict()
 
     # grab sceneids from the file in input_product_list field
     _ipl_str = request.files.get('input_product_list').read()
-    _splitter = "\n" if "\n" in _ipl_str else "\r"
-    _ipl = _ipl_str.split(_splitter)
+    _ipl = _ipl_str.split("\n" if "\n" in _ipl_str else "\r")
 
-    # use available-products to convert our list of sceneids into
-    # the format required for new orders
-    ap_url = api_base_url + "/api/v0/available-products"
-    ap_resp = requests.post(ap_url, json={'inputs': _ipl}, auth=(user.username, user.wurd))
-    scene_dict_all_prods = ap_resp.json()
+    # convert our list of sceneids into format required for new orders
+    scene_dict_all_prods = api_post("/api/v0/available-products", {'inputs': _ipl}).json()
 
     # create a list of requested products
-    product_list = []
-    for key in data.keys():
-        if key in conversions['products'].keys():
-            product_list.append(key)
-            # now that we have the product list, lets remove
-            # this key from the form inputs
-            data.pop(key)
+    product_list = [key for key in data.keys() if key in conversions['products'].keys()]
+    # now that we have the product list, lets remove
+    # this key from the form inputs
+    for p in product_list:
+        data.pop(p)
 
-    # pop 'image_extents' if present.
     # the image extents parameters also come in under
     # this key in the form, and this causes a conflict
     # with the 'image_extents' used to enable modifying
@@ -146,16 +147,7 @@ def submit_order():
         # all values coming in from the post request
         # are unicode, convert those values which
         # should be int or float
-        val = is_num(v)
-        if "|" not in k:
-            tdict = {k: val}
-        else:
-            key_list = k.split("|")
-            if len(key_list) == 3:
-                tdict = {key_list[0]: {key_list[1]: {key_list[2]: val}}}
-            else:
-                tdict = {key_list[0]: {key_list[1]: val}}
-
+        tdict = gen_nested_dict(k.split("|"), is_num(v))
         # deep_update updates the dictionary
         deep_update(out_dict, tdict)
 
@@ -177,17 +169,13 @@ def submit_order():
             if item == 'target_projection' and out_dict[item] == 'lonlat':
                 # there are no other parameters needed for a geographic
                 # projection, so assign 'lonlat' to the projection key
-                out_dict['projection'] = 'lonlat'
+                out_dict['projection'] = {'lonlat': None}
             out_dict.pop(item)
 
     if 'plot_statistics' in out_dict.keys():
         out_dict['plot_statistics'] = True
 
-    print "****** out_dict", out_dict
-
-
-    url = api_base_url + "/api/v0/order"
-    response = requests.post(url, json=out_dict, auth=(user.username, user.wurd))
+    response = api_post("/api/v0/order", out_dict)
     response_data = response.json()
 
     # hack till we settle on msg or message
@@ -195,41 +183,37 @@ def submit_order():
         response_data['msg'] = response_data['message']
 
     if response.status_code == 200:
-        flash("Order submitted successfully! Your OrderId is {}".format(response_data))
-        return redirect('/ordering/order-status/{}/'.format(response_data['orderid']))
+        flash("Order submitted successfully! Your OrderId is {}".format(response_data['orderid']))
+        rdest = redirect('/ordering/order-status/{}/'.format(response_data['orderid']))
     else:
-        flash("There was an issue submitting your order. {}".format(response_data["msg"]), 'error')
-        return redirect(url_for('new_order'))
+        flash(format_errors(["msg"]), 'error')
+        rdest = redirect(url_for('new_order'))
+
+    return rdest
+
 
 @app.route('/ordering/status/')
 @app.route('/ordering/status/<email>/')
 @login_required
 def list_orders(email=None):
-    user = session['user']
-    url = api_base_url + "/api/v0/list-orders-ext"
+    url = "/api/v0/list-orders-ext"
     if email:
-        url += "/{0}".format(email)
-    json = {'status': ['complete', 'ordered']}
-    response = requests.get(url, json=json, auth=(user.username, user.wurd))
-    res_data = response.json()
-    return render_template('list_orders.html', user=user, order_list=res_data)
+        url += "/{}".format(email)
+    res_data = api_get(url, json={'status': ['complete', 'ordered']})
+    return render_template('list_orders.html', order_list=res_data)
+
 
 @app.route('/ordering/order-status/<orderid>/')
 @login_required
 def view_order(orderid):
-    user = session['user']
+    order_dict = api_get("/api/v0/order/{}".format(orderid))
 
-    order_dict_url = api_base_url + "/api/v0/order/{}".format(orderid)
-    order_dict = requests.get(order_dict_url, auth=(user.username, user.wurd)).json()
-
-    scenes_url = api_base_url + "/api/v0/item-status/{}".format(orderid)
-    scenes_resp = requests.get(scenes_url, auth=(user.username, user.wurd)).json()
+    scenes_resp = api_get("/api/v0/item-status/{}".format(orderid))
     scenes = scenes_resp['orderid'][orderid]
 
-    complete = ['complete', 'unavailable']
-    open = ['oncache', 'queued', 'processing', 'error', 'submitted']
-    waiting = ['retry', 'onorder']
-    statuses = {'complete': complete, 'open': open, 'waiting': waiting}
+    statuses = {'complete': ['complete', 'unavailable'],
+                'open': ['oncache', 'queued', 'processing', 'error', 'submitted'],
+                'waiting': ['retry', 'onorder']}
 
     product_counts = {}
     for status in statuses:
@@ -242,93 +226,75 @@ def view_order(orderid):
     # sensor/products
     options_by_sensor = {}
     for key in order_dict['product_opts']:
-        if isinstance(order_dict['product_opts'][key], dict) and 'products' in order_dict['product_opts'][key].keys():
-            _spl = order_dict['product_opts'][key]['products']
+        _kval = order_dict['product_opts'][key]
+        if isinstance(_kval, dict) and 'products' in _kval.keys():
+            _spl = _kval['products']
             _out_spl = [conversions['products'][item] for item in _spl]
-
             options_by_sensor[key] = _out_spl
 
-
-    options_list = []
-    for sensor in options_by_sensor:
-        options_list.append(" {0}: {1} ,".format(sensor, ", ".join(options_by_sensor[sensor])))
-
+    options_list = [" {0}: {1} ,".format(sensor, ", ".join(options_by_sensor[sensor])) for sensor in options_by_sensor]
     options_str = " ".join(options_list)
 
     return render_template('view_order.html', order=order_dict, scenes=scenes, prod_str=options_str,
-                           product_counts=product_counts, user=user, product_opts=joptions)
+                           product_counts=product_counts, product_opts=joptions)
+
 
 @app.route('/reports/')
 @login_required
 def list_reports():
-    user = session['user']
-    req_url = api_base_url + "/api/v0/reports/"
-    response = requests.get(req_url, auth=(user.username, user.wurd))
-    res_data = response.json()
-    return render_template('list_reports.html', reports=res_data, user=user)
+    res_data = api_get("/api/v0/reports/")
+    return render_template('list_reports.html', reports=res_data)
+
 
 @app.route('/reports/<name>/')
 @login_required
 def show_report(name):
-    user = session['user']
-    req_url = api_base_url + "/api/v0/reports/{0}/".format(name)
-    response = requests.get(req_url, auth=(user.username, user.wurd))
-    res_data = eval(response.json())
+    response = api_get("/api/v0/reports/{0}/".format(name))
+    res_data = eval(response)
+    return render_template('report.html', report_name=name, report=res_data)
 
-    return render_template('report.html', report_name=name, report=res_data, user=user)
 
 @app.route('/console', methods=['GET'])
 @login_required
 def console():
-    user = session['user']
-    req_url = api_base_url + "/api/v0/statistics/all"
-    response = requests.get(req_url, auth=(user.username, user.wurd))
-    data = response.json()
+    data = api_get("/api/v0/statistics/all")
     stats = {'Open Orders': data['stat_open_orders'],
              'Products Complete 24hrs': data['stat_products_complete_24_hrs'],
              'Waiting Users': data['stat_waiting_users'],
              'Backlog Depth': data['stat_backlog_depth']}
-    return render_template('console.html', user=user, stats=stats)
+    return render_template('console.html', stats=stats)
+
 
 @app.route('/console/statusmsg', methods=['GET', 'POST'])
 @login_required
 def statusmsg():
-    user = session['user']
     if request.method == 'POST':
-        req_url = api_base_url + '/api/v0/system-status-update'
-        dsm = 'False'
-        if 'display_system_message' in request.form.keys():
-            dsm = 'True'
+        dsm = 'True' if 'display_system_message' in request.form.keys() else 'False'
         api_args = {'system_message_title': request.form['system_message_title'],
                     'system_message_body': request.form['system_message_body'],
                     'display_system_message': dsm}
-        response = requests.post(req_url,
-                                 data=json.dumps(api_args),
-                                 auth=(user.username, user.wurd))
+        response = api_post('/api/v0/system-status-update', api_args)
+
         if response.status_code == 200:
             update_status_details()
             flash('update successful')
-            return redirect(url_for('index'))
+            rurl = 'index'
         else:
             flash('update failed', 'error')
-            return redirect(url_for('statusmsg'))
+            rurl = 'statusmsg'
+        action = redirect(url_for(rurl))
     else:
         update_status_details()
-        return render_template('statusmsg.html',
-                               user=user,
-                               display_system_message=session['display_system_message'],
-                               system_message_title=session['system_message_title'],
-                               system_message_body=session['system_message_body'])
+        action = render_template('statusmsg.html')
+
+    return action
+
 
 @app.route('/console/config', methods=['GET'])
 @login_required
 def console_config():
-    user = session['user']
-    req_url = api_base_url + "/api/v0/system/config"
-    response = requests.get(req_url, auth=(user.username, user.wurd))
-    config_data = response.json()
-    return render_template('config.html', user=user, config_data=config_data)
+    config_data = api_get("/api/v0/system/config")
+    return render_template('config.html', config_data=config_data)
 
 if __name__ == '__main__':
-
     app.run(debug=True, use_evalex=False, host='0.0.0.0', port=8889)
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,16 @@
 import collections
 
+
+class User(object):
+    def __init__(self, **kwargs):
+        for k, v in kwargs.iteritems():
+            self.__setattr__(k, v)
+
+    @property
+    def is_staff(self):
+        return 'staff' in self.roles
+
+
 def deep_update(source, overrides):
     """
     update a nested dictionary. modify source in place
@@ -27,13 +38,29 @@ def is_num(value):
             # is it a float
             rv = float(value)
         except:
-            # must be a str
+            # then return a str
             rv = value
     return rv
 
+
+def gen_nested_dict(inlist, val):
+    inlist.reverse()
+    for idx, item in enumerate(inlist):
+        _indict = {item: val} if idx is 0 else {item: _indict}
+    return _indict
+
+
+def format_errors(indict):
+    outlist = []
+    for key in indict:
+        outlist.append(key+"<br/>")
+        for item in indict[key]:
+            outlist.append("&#8594;"+item+"<br/>")
+    return "".join(outlist)
+
 conversions = {
-    'products':
-        {'l1': 'original input products',
+    'products': {
+         'l1': 'original input products',
          'swe': 'dynamic surface water extent',
          'sr_evi': 'sr_evi',
          'cloud': 'cfmask',
@@ -48,57 +75,6 @@ conversions = {
          'bt': 'brightness temperature',
          'toa': 'top of atmosphere',
          'stats': 'plots and statistics',
-         'sr_ndvi': 'sr_ndvi'},
-
-
-    'products_prev':
-        {'include_source_data': 'l1',
-         'include_dswe': 'swe',
-         'include_sr_evi': 'sr_evi',
-         'include_cfmask': 'cloud',
-         'include_sr_savi': 'sr_savi',
-         'include_sr_nbr2': 'sr_nbr2',
-         'include_sr_nbr': 'sr_nbr',
-         'include_sr_ndmi': 'sr_ndmi',
-         'include_sr': 'sr',
-         'include_sr_msavi': 'sr_msavi',
-         'include_lst': 'lst',
-         'include_source_metadata': 'source_metadata',
-         'include_sr_thermal': 'bt',
-         'include_sr_toa': 'toa',
-         'include_statistics': 'stats',
-         'include_sr_ndvi': 'sr_ndvi'},
-
-
-    'aea_map':
-        {'central_meridian': 'central_meridian',
-         'std_parallel_1': 'standard_parallel_1',
-         'std_parallel_2': 'standard_parallel_2',
-         'datum': 'datum',
-         'false_northing': 'false_northing',
-         'false_easting': 'false_easting',
-         'origin_lat': 'latitude_of_origin'},
-
-    'ps_map':
-        {'longitude_pole': 'longitudinal_pole',
-         'latitude_true_scale': 'latitude_true_scale'},
-
-    'utm_map':
-        {'utm_zone': 'zone',
-         'utm_north_south': 'zone_ns'},
-
-    'ext_map':
-        {'image_extents_units': 'units',
-         'minx': 'west',
-         'miny': 'south',
-         'maxy': 'north',
-         'maxx': 'east'},
-
-    'keywords_map':
-        {'resample_method': 'resampling_method',
-         'output_format': 'format',
-         'image_extents': 'image_extents',
-         'resize': 'resize',
-         'reproject': 'projection'},
-
+         'sr_ndvi': 'sr_ndvi'
+    }
 }
