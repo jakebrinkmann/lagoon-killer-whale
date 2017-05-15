20bd349f9f030e890d1fd2116427e5dd14eacb70
Jake Brinkmann <jakebrinkmann@gmail.com>
2017-05-15 13:55:40 -0500

    ENH: add pixel_qa option (staff only)

--- src/utils.py
+++ src/utils.py
@@ -71,6 +71,7 @@ conversions = {
          'l1': 'original input products',
          'swe': 'dynamic surface water extent',
          'sr_evi': 'sr_evi',
+         'pixel_qa': 'pixel_qa',
          'sr_savi': 'sr_savi',
          'sr_nbr2': 'sr_nbr2',
          'sr_nbr': 'sr_nbr',
