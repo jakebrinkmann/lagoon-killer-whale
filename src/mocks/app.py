f6e6adf4d1072dce32169513c7fb1b03d3db8810
Jake Brinkmann <jakebrinkmann@gmail.com>
2017-05-23 13:37:40 -0500

    TST: provide no orders for now

--- src/mocks/app.py
+++ src/mocks/app.py
@@ -19,12 +19,7 @@ def api_up_user_fail(url, response_type='json', json=None, uauth=None):
 
 
 def api_up_list_orders(*args, **kwargs):
-    url = args[0]
-    if url == '/list-orders':
-        return []
-    return [{'orderid': 'abc', 'products_ordered': 1,
-             'products_complete': 1, 'order_status': 'complete',
-             'order_note': 'comments'}]
+    return []
 
 
 def api_up_order_status(*args):
