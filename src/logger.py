7b11f0b49e154a6cd3277a5c5d803c926122fb15
Jake Brinkmann <jakebrinkmann@gmail.com>
2017-07-22 23:05:39 -0500

    BLD: configure stdout logging

--- src/logger.py
+++ src/logger.py
@@ -1,7 +1,8 @@
 import os
+import sys
 import logging
 
-from logging import FileHandler
+from logging import StreamHandler
 from logging import Formatter
 from logging import Filter
 from logging.handlers import SMTPHandler
@@ -9,26 +10,21 @@ from logging.handlers import SMTPHandler
 LOG_FORMAT = ('%(asctime)s [%(levelname)s]: %(message)s in %(pathname)s:%(lineno)d')
 
 
-class DbgFilter(Filter):
-    def filter(self, rec):
-        return rec.levelno == logging.DEBUG
-
 ilogger = logging.getLogger("espa-web")
 ilogger.setLevel(logging.DEBUG)
 
-ih = FileHandler("/var/log/uwsgi/espa-web-info.log")
-dh = FileHandler("/var/log/uwsgi/espa-web-debug.log")
-eh = SMTPHandler(mailhost='localhost', fromaddr='espa@usgs.gov', toaddrs='gs-n-edc_espa_api@usgs.gov', subject='ESPA WEB ERROR')
+ih = StreamHandler(stream=sys.stdout)
+eh = SMTPHandler(mailhost='localhost', fromaddr='espa@usgs.gov', toaddrs=os.getenv('ESPA_WEB_EMAIL_RECEIVE').split(','), subject='ESPA WEB ERROR')
 
-ih.setLevel(logging.INFO)
-dh.setLevel(logging.DEBUG)
-eh.setLevel(logging.DEBUG)
+ih.setLevel(logging.DEBUG)
+eh.setLevel(logging.CRITICAL)
 
 for handler in [ih, dh, eh]:
     ilogger.addHandler(handler)
 
-    if isinstance(handler, logging.FileHandler):
+    if isinstance(handler, logging.StreamHandler):
         handler.setFormatter(Formatter(LOG_FORMAT))
 
     if handler.level == logging.DEBUG:
         handler.addFilter(DbgFilter())
+
