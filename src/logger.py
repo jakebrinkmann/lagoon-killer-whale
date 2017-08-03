93a06260251c96140fb60ebd6b4f96d410edd934
Jake Brinkmann <jakebrinkmann@gmail.com>
2017-08-03 10:05:53 -0500

    MAINT: fix logging for production env

--- src/logger.py
+++ src/logger.py
@@ -2,7 +2,8 @@ import os
 import sys
 import logging
 
-from logging import StreamHandler
+
+from logging import StreamHandler, FileHandler
 from logging import Formatter
 from logging import Filter
 from logging.handlers import SMTPHandler
@@ -13,8 +14,14 @@ LOG_FORMAT = ('%(asctime)s [%(levelname)s]: %(message)s in %(pathname)s:%(lineno
 ilogger = logging.getLogger("espa-web")
 ilogger.setLevel(logging.DEBUG)
 
-ih = StreamHandler(stream=sys.stdout)
-eh = SMTPHandler(mailhost='localhost', fromaddr='espa@usgs.gov', toaddrs=os.getenv('ESPA_WEB_EMAIL_RECEIVE').split(','), subject='ESPA WEB ERROR')
+espa_log_dir = os.getenv('ESPA_LOG_DIR')
+if espa_log_dir:
+    ih = FileHandler(os.path.join(espa_log_dir, "espa-web-flask.log"))
+else:
+    ih = StreamHandler(stream=sys.stdout)
+
+receivers = os.getenv('ESPA_WEB_EMAIL_RECEIVE', 'gs-n-edc_espa_api@usgs.gov').split(',')
+eh = SMTPHandler(mailhost='localhost', fromaddr='espa@usgs.gov', toaddrs=receivers, subject='ESPA WEB ERROR')
 
 if os.getenv('ESPA_ENV') == 'dev':
     ih.setLevel(logging.DEBUG)
