ac5922cd90e833fe4b2f40a5eedf60a3d9e7b0f3
Jake Brinkmann <jakebrinkmann@gmail.com>
2017-10-17 10:08:39 -0500

    MAINT: typo on flash

--- src/app.py
+++ src/app.py
@@ -112,7 +112,7 @@ def login_required(f):
         logged_in = (('logged_in' in session and session['logged_in'] is True)
                      and request.cookies.get(SSO_COOKIE_NAME))
         if not logged_in:
-            flash(format_messages('Login Required', 'error')
+            flash('Login Required', 'error')
             return redirect(url_for('index', next=request.full_path))
         return f(*args, **kwargs)
     return decorated_function
