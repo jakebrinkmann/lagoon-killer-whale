eb04074d1b41892caa9f3207de64ad41781e8f1a
Jake Brinkmann <jakebrinkmann@gmail.com>
2018-04-04 00:11:17 -0500

    DEP: back to basic vue js webpack

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,19 +1,15 @@
-/* URI Request Routing */
 import Vue from 'vue'
 import Router from 'vue-router'
-import Hello from '@/components/Hello'
+import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
-let router = new Router({
-  mode: 'history',
+export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: Hello
+      name: 'HelloWorld',
+      component: HelloWorld
     }
   ]
 })
-
-export default router
